d5c564c4e796a349b3de0cf1a728947188f3d425
S-Marimbi <marimbinjunge@gmail.com>
2024-06-17 12:04:19 +0300

    Completed gameloop

---
+++ lib/gameloop.py
@@ -0,0 +1,10 @@
+#intialaizing the game
+#initializing the board,tile bag, and players
+
+#Function to start the game
+def start_game():
+    board = initialize_board()
+    tile_bag = initialize_tile_bag()
+    players = [Player('Human', tile_bag), Player('Computer', tile_bag)]
+    current_player = 0
+    return board, tile_bag, players, current_player            
